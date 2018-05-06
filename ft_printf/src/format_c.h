083d7a8cbea7513cf2f6ad209f70f1958668a267
p.leroux <pleroux@student.42.fr>
2018-05-06 08:57:18 +0200

    nr ft_printf

--- ft_printf/src/format_c.h
+++ ft_printf/src/format_c.h
@@ -6,7 +6,7 @@
 /*   By: pleroux <pleroux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/30 16:35:24 by pleroux           #+#    #+#             */
-/*   Updated: 2018/01/30 16:44:17 by pleroux          ###   ########.fr       */
+/*   Updated: 2018/05/06 08:56:56 by pleroux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # include <wchar.h>
 # include "../inc/ft_printf.h"
 
-wint_t				cast_format_c(t_format *t);;;;
+wint_t				cast_format_c(t_format *t);
 char				*param_attribut_c(t_format *t, char *s);
 char				*conv_format_c(t_format *t);
 
